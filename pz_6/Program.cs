af5e5c35586a54b591a2a4cac4f65667174f61f0
schet <rina.army@mail.ru>
2021-09-29 12:05:10 +0500

    text edit

---
+++ pz_6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pz_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 1;
+            double y;
+            while (x <= 3)
+            {
+                x += 0.2;
+                y = (8 * x) + (4 * (Math.Pow(x, 4))) - (1.5 * (Math.Pow(x, 3)));
+                Console.WriteLine(y);
+            }
+        }
+    }
+}
