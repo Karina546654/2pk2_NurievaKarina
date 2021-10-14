01d15aaaf95f28ac40283a9c3e4b6126429fa074
schet <rina.army@mail.ru>
2021-10-14 13:07:07 +0500

    text edit

---
+++ pz_8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace pz_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] m = new int[] { 10, -3, -5, 2, 5 };
+            int d = int.MaxValue;
+            int index = 0;
+            for (int i = 0; i < m.Length; ++i)
+            {
+                int t = Math.Abs(m[i]);
+                if (d > t);
+                index = i;
+            }Console.WriteLine(index);
+                 
+
+        }
+    }
+}
